e2bc3259f0b3bb9842f7d15a91abea67b14c68e8
Edelis Molina <edelitamr@gmail.com>
2021-07-24 17:01:16 -0400

    Adding CSS

--- src/main/java/ucf/assignments/InventoryManager.java
+++ src/main/java/ucf/assignments/InventoryManager.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class InventoryManager extends Application {
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Inventory Tracker Application");
+            primaryStage.getIcons().add(new Image("ucf/assignments/index.png"));
             primaryStage.show();
 
         } catch (IOException e) {
